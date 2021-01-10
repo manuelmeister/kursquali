52926e4d5f823909acf6e0ea839753a58b49fcf1
Manuel Meister <news.manuelsworld@gmail.com>
2021-01-10 20:27:21 +0100

    Add Period

--- api/src/Entity/Camp.php
+++ api/src/Entity/Camp.php
@@ -31,9 +31,15 @@ class Camp
      */
     private $campCollaborations;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Period::class, mappedBy="camp", orphanRemoval=true)
+     */
+    private $periods;
+
     public function __construct()
     {
         $this->campCollaborations = new ArrayCollection();
+        $this->periods = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -77,6 +83,36 @@ class Camp
             // set the owning side to null (unless already changed)
             if ($campCollaboration->getCamp() === $this) {
                 $campCollaboration->setCamp(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Period[]
+     */
+    public function getPeriods(): Collection
+    {
+        return $this->periods;
+    }
+
+    public function addPeriod(Period $period): self
+    {
+        if (!$this->periods->contains($period)) {
+            $this->periods[] = $period;
+            $period->setCamp($this);
+        }
+
+        return $this;
+    }
+
+    public function removePeriod(Period $period): self
+    {
+        if ($this->periods->removeElement($period)) {
+            // set the owning side to null (unless already changed)
+            if ($period->getCamp() === $this) {
+                $period->setCamp(null);
             }
         }
 
---
+++ api/src/Entity/Period.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PeriodRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=PeriodRepository::class)
+ */
+class Period
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $start;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $end;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Camp::class, inversedBy="periods")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $camp;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStart(): ?\DateTimeInterface
+    {
+        return $this->start;
+    }
+
+    public function setStart(\DateTimeInterface $start): self
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getEnd(): ?\DateTimeInterface
+    {
+        return $this->end;
+    }
+
+    public function setEnd(\DateTimeInterface $end): self
+    {
+        $this->end = $end;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCamp(): ?Camp
+    {
+        return $this->camp;
+    }
+
+    public function setCamp(?Camp $camp): self
+    {
+        $this->camp = $camp;
+
+        return $this;
+    }
+}
