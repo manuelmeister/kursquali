2edfd885075073fabb151c12bef6f7aec016dc4d
Manuel Meister <news.manuelsworld@gmail.com>
2021-01-10 19:36:01 +0100

    Add main layout to app

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <v-app>
     <app-navigation/>
-
-    <v-main>
-      <HelloWorld />
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col>
+            <router-view/>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 import AppNavigation from "@/components/AppNavigation.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    AppNavigation,
-    HelloWorld
+    AppNavigation
   },
 
   data: () => ({
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-card>
+    <v-card-title><h1>{{ $tc("views.home.welcome") }}</h1></v-card-title>
+  </v-card>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import {Component, Vue} from "vue-property-decorator";
 
 @Component({
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+}
 </script>
